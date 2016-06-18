eeb02ca1ed524cdb446a2ab2e43b66462056bff1
Kelly Dwan <kelly.dwan@gmail.com>
2016-06-18 13:43:49 -0400

    Export all the functions from selector files

--- state/selectors/index.js
+++ state/selectors/index.js
@@ -1,6 +1,6 @@
-import comments from './comments';
-import pages from './pages';
-import posts from './posts';
-import terms from './terms';
+import * as comments from './comments';
+import * as pages from './pages';
+import * as posts from './posts';
+import * as terms from './terms';
 
 export default Object.assign( {}, comments, pages, posts, terms );
